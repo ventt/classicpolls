007665bb087d8fbb20f9bdbc692c2547a48ea0aa
Adrian Szell <adrian.szell@scademy.com>
2025-10-28 21:39:31 +0100

    remove redundant global css and refactor layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Providers } from "./providers";
 import type { Metadata } from "next";
+import React from "react";
+
 
 export const metadata: Metadata = {
     title: "Classic Polls",
@@ -10,20 +12,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body
-            className="min-h-screen text-gray-100 bg-cover bg-center bg-fixed"
-            style={{
-                backgroundImage: "url('/wallpaper.jpg')",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundAttachment: "fixed",
-            }}
-        >
-
-        <div className="min-h-screen bg-black/70 backdrop-blur-sm">
-                <Providers>{children}</Providers>
-        </div>
-        </body>
+            <body className="min-h-screen text-gray-100 bg-cover bg-center bg-fixed overflow-hidden"
+                style={{
+                    backgroundImage: "url('/wallpaper.jpg')",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundAttachment: "fixed",
+                }}>
+                <div className="min-h-screen bg-black/70 backdrop-blur-sm">
+                        <Providers>{children}</Providers>
+                </div>
+            </body>
         </html>
     );
 }
