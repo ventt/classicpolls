8d964bb3536e10cc004084d3854984e593a6d1d6
Andras Szell <andras@szell.dev>
2025-10-29 13:12:33 +0100

    WIP - fix api URL

--- frontend/lib/postgrest.ts
+++ frontend/lib/postgrest.ts
@@ -5,7 +5,7 @@ import {PostgrestClient} from '@supabase/postgrest-js'
  * @param jwtToken
  */
 export function postgrest(jwtToken?: string) {
-    return new PostgrestClient(process.env.NEXT_PUBLIC_API_URL_POSTGREST!, {
+    return new PostgrestClient(process.env.API_URL_POSTGREST!, {
         // @ts-ignore
         headers: {...jwtToken ? {Authorization: `Bearer ${jwtToken}`} : {}},
     })
