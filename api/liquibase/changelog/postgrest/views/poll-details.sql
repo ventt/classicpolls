5db98dbb30223370c3eeb60b8912e615d32a6de9
Adrian Szell <adrian.szell@scademy.com>
2025-10-30 13:52:33 +0100

    Add user_votes to poll_details view

--- api/liquibase/changelog/postgrest/views/poll-details.sql
+++ api/liquibase/changelog/postgrest/views/poll-details.sql
@@ -13,20 +13,25 @@ WITH upvotes_count AS (SELECT pv.poll_id,
                                 COUNT(*) AS downvotes
                          FROM api.vote pv
                          WHERE pv.choice = FALSE
-                         GROUP BY pv.poll_id)
+                         GROUP BY pv.poll_id),
+     user_votes AS (SELECT poll_id, choice
+                    FROM api.vote
+                    WHERE user_sub = jwt_sub())
 SELECT p.id,
        p.title,
        p.description,
        p.category_name,
-       COALESCE(uc.upvotes, 0)                                               AS upvotes,
-       COALESCE(dc.downvotes, 0)                                             AS downvotes,
-       COALESCE(uc.upvotes, 0) + COALESCE(dc.downvotes, 0)                   AS total_votes,
+       uv.choice                                                                 AS user_choice,
+       COALESCE(uc.upvotes, 0)                                                   AS upvotes,
+       COALESCE(dc.downvotes, 0)                                                 AS downvotes,
+       COALESCE(uc.upvotes, 0) + COALESCE(dc.downvotes, 0)                       AS total_votes,
        (COALESCE(uc.upvotes, 0)::DECIMAL / GREATEST(dc.downvotes, 1)::DECIMAL) /
-       (COALESCE(uc.upvotes, 0) + COALESCE(dc.downvotes, 0))::DECIMAL        AS upvote_ratio,
-       COALESCE(uc.upvotes, 0)::DECIMAL / GREATEST(dc.downvotes, 1)::DECIMAL AS approval_score
+       GREATEST(COALESCE(uc.upvotes, 0) + COALESCE(dc.downvotes, 0), 1)::DECIMAL AS upvote_ratio,
+       COALESCE(uc.upvotes, 0)::DECIMAL / GREATEST(dc.downvotes, 1)::DECIMAL     AS approval_score
 FROM api.poll p
          LEFT JOIN upvotes_count uc ON p.id = uc.poll_id
-         LEFT JOIN downvotes_count dc ON p.id = dc.poll_id;
+         LEFT JOIN downvotes_count dc ON p.id = dc.poll_id
+         LEFT JOIN user_votes uv ON p.id = uv.poll_id;
 
 GRANT SELECT ON TABLE api.poll_details TO web_anon;
 GRANT SELECT ON TABLE api.poll_details TO web_user;
