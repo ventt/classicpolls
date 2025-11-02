bfef2ce3b1937206b39d4e4f2af96ca40be5e462
Adrian Szell <adrian.szell@scademy.com>
2025-11-02 23:55:55 +0100

    fix(background): replace style with tailwind add scrollbar to main page body and add ios safari themeColor

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 import React from "react";
 
 
@@ -7,22 +7,26 @@ export const metadata: Metadata = {
     title: "Classic Polls - The Classic+ voting site",
     alternates: {canonical: "/"},
     description: "Community-created Classic+ vision polls allowing players to vote on feature ideas and see how the wider WoW community feels about them.",
-    appleWebApp: {capable: true, title: "Classic Polls", statusBarStyle: "black-translucent"}
+    appleWebApp: {capable: true, title: "Classic Polls", statusBarStyle: "black-translucent"},
 };
+export const viewport: Viewport = {
+    themeColor: "#130a03"
+}
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className="min-h-screen bg-cover bg-center bg-fixed"
-              style={{
-                  backgroundImage: "url('/wallpaper.webp')",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                  backgroundAttachment: "fixed",
-              }}>
-        <div className="min-h-screen bg-black/70 backdrop-blur-sm">
+        <body className="bg-gradient-to-t from-lime-900 via-emerald-800 to-yellow-950 sm:bg-[url(/wallpaper.webp)] sm:bg-cover sm:bg-center sm:bg-fixed sm:bg-no-repeat
+                            overflow-y-auto scrollbar scrollbar-thumb-rounded scrollbar-thumb-emerald-900 scrollbar-track-rounded scrollbar-track-zinc-900"
+        >
+        <div className="bg-black/70 backdrop-blur-sm ">
             {children}
         </div>
         </body>
         </html>
     );
 }
+// 19 10 3
+// yellow-950
+// emerald-300
+// lime-300
+//67,47,21
