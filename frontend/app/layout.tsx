c67f8d79507a86b138ab5bc82a7cf36afd4ea649
Adrian Szell <adrian.szell@scademy.com>
2025-11-03 02:56:57 +0100

    fix(padding) - fix padding

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         >
         <div className="min-h-screen bg-black/60 lg:bg-black/70 backdrop-blur-sm">
             <div
-                className="flex flex-col w-full pr-2 pl-2 md:pr-4 md:pl-4 lg:pr-0 lg:pl-0 lg:m-0 lg:w-[70%] gap-2 justify-self-center">
+                className="flex flex-col w-full pt-1 md:pt-4 lg:pt-2 pr-2 pl-2 md:pr-4 md:pl-4 lg:pr-0 lg:pl-0 lg:m-0 lg:w-[68%] gap-2 justify-self-center">
                 <SiteHeader/>
-                <main className="flex flex-col gap-4 pt-2 pb-6 lg:pb-0">
+                <main className="flex flex-col gap-4 pt-1 lg:pt-2 pb-6 lg:pb-0">
                     {children}
                 </main>
             </div>
