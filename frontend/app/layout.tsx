f3356b6b6d01da36b7e520276fa9077cc9bdeb0f
Adrian Szell <adrian.szell@scademy.com>
2025-12-12 16:44:14 +0100

    fix(bg + seo): fix safari white bars + canonical url

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,22 +5,26 @@ import SiteHeader from "@/components/SiteHeader";
 
 export const metadata: Metadata = {
     title: "Classic Polls - The Classic+ voting site",
-    alternates: {canonical: "/"},
+    alternates: {canonical: "https://classicpolls.com/"},
     description: "Community-created Classic+ vision polls allowing players to vote on feature ideas and see how the wider WoW community feels about them. Classic+ survey.",
     appleWebApp: {capable: true, title: "Classic Polls", statusBarStyle: "black-translucent"},
 };
 export const viewport: Viewport = {
     width: "device-width",
-    themeColor: "#130a03",
-    initialScale: 1.0
+    themeColor: [
+        {media: "(prefers-color-scheme: dark)", color: "#130a03"},
+        {media: "(prefers-color-scheme: light)", color: "#130a03"}
+    ],
+    initialScale: 1.0,
+
 }
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className="min-h-screen bg-gradient-to-t from-yellow-950 via-emerald-900 to-yellow-950 lg:bg-[url(/wallpaper.avif)] lg:bg-cover sm:bg-center lg:bg-fixed lg:bg-no-repeat
+        <body className="min-h-screen bg-[#130a03] lg:bg-[url(/wallpaper.avif)] lg:bg-cover lg:bg-fixed lg:bg-no-repeat
                             overflow-y-auto scrollbar scrollbar-thumb-rounded scrollbar-thumb-emerald-900 scrollbar-track-rounded scrollbar-track-zinc-900"
         >
-        <div className="min-h-screen bg-black/60 lg:bg-black/70 backdrop-blur-sm">
+        <div className="min-h-screen  lg:bg-black/70 lg:backdrop-blur-sm">
             <div
                 className="flex flex-col w-full pt-1 md:pt-4 lg:pt-2 pr-2 pl-2 md:pr-4 md:pl-4 lg:pr-0 lg:pl-0 lg:mx-auto lg:w-[68%] gap-2 justify-self-center">
                 <SiteHeader/>
